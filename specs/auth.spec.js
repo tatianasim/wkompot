ed8ec1a672e8e7d7d6c784568148612c07970690
Tatiana <s_tvl@mail.ru>
2021-12-08 13:13:25 +0200

    add hook in auth.spec.js

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -2,8 +2,11 @@ import LoginPage from '../pages/login.page';
 import ProfilePage from '../pages/profile.page';
 
 describe('Auth', function () {
-    it('Successful log in', async function() {
+    beforeEach(async function() {
         await LoginPage.open();
+    });
+
+    it('Successful log in', async function() {
         await expect(LoginPage.buttonSubmit).toBeDisabled();
         await LoginPage.login('test@example.com', 'Qwerty!23');
         await expect(ProfilePage.iconUser).toBeDisplayed();
