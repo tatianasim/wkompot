450e87dc7289581d7bfb93f6a42e13ad6bb3e403
Tatiana <s_tvl@mail.ru>
2021-12-09 13:21:47 +0200

    add environment variables

--- pages/login.page.js
+++ pages/login.page.js
@@ -5,6 +5,9 @@ class LoginPage extends Page {
     get inputUsername() { return $('[qa-id="email"]'); }
     get inputPassword() { return $('[qa-id="password"]'); }
     get buttonSubmit() { return $('.login-form-button'); }
+    get notification() { return $('.ant-notification-notice-message')};
+    get loginError() { return $('//div[@data-qa="normalEmail"]//div[@class="ant-form-item-explain-error"]')}
+    get passwordError() { return $('//div[div[@class="ant-form-item-control-input"]//input[@qa-id="password"]]//div[@class="ant-form-item-explain-error"]')}
 
     async login(username, password) {
         await this.inputUsername.setValue(username);
