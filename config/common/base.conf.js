d64b081dc1e20beb0b635c636121098c5f322b58
Tatiana <s_tvl@mail.ru>
2021-12-13 18:30:53 +0200

    update config

---
+++ config/common/base.conf.js
@@ -0,0 +1,31 @@
+import { config } from 'dotenv';
+import hooks from './hooks.conf';
+
+config();
+
+exports.config = {
+    specs: [
+        './specs/**/*.spec.js'
+    ],
+    exclude: [
+    ],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: process.env.BASE_URL,
+
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 30000,
+    connectionRetryCount: 1,
+
+    services: ['devtools', 'geckodriver'],
+    reporters: ['spec', ['allure', {
+        disableWebdriverStepsReporting: true
+    }]],
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+    ...hooks,
+}
